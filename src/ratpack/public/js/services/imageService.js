8dec13faea81fd259db78f8487a20b8b008fcbcc
Sean Walsh <sean@swalsh.org>
2014-09-19 17:55:57 -0700

    Refactor imageService so that I don't have to write $q.defer() on every method.

--- src/ratpack/public/js/services/imageService.js
+++ src/ratpack/public/js/services/imageService.js
@@ -3,42 +3,50 @@
 angular.module( "uploader.services" )
 
 .service( "imageService", [ "$q", "$http", function( $q, $http ) {
+
+	var promise = function( callback ) {
+
+		var deferred = $q.defer();
+
+		callback( deferred );
+
+		return deferred.promise;
+
+	};
+
 	return {
 
 		getImages: function() {
+			return promise(function( deferred ) {
 
-			var deferred = $q.defer();
+				$http.get( "image" )
+					.success( function( data ) {
+						deferred.resolve( data );
+					})
+					.error( function( error ) {
+						deferred.reject( error );
+					});
 
-			$http.get( "image" )
-				.success( function( data ) {
-					deferred.resolve( data );
-				})
-				.error( function( error ) {
-					deferred.reject( error );
-				});
-
-			return deferred.promise;
-
+			});
 		},
 
 		uploadImage: function( image ) {
+			return promise( function( deferred ) {
 
-			var deferred = $q.defer(),
-				formData = new FormData();
+				var formData = new FormData();
 
-			formData.append( "fileUpload", image );
+				formData.append( "fileUpload", image );
 
-			$http.post( "image/upload", formData, {
-				transformRequest: angular.identity,
-				headers: { "Content-Type": undefined }
-			}).success( function( data ) {
-				deferred.resolve( data );
-			}).error( function( error ) {
-				deferred.reject( error );
+				$http.post( "image/upload", formData, {
+					transformRequest: angular.identity,
+					headers: { "Content-Type": undefined }
+				}).success( function( data ) {
+					deferred.resolve( data );
+				}).error( function( error ) {
+					deferred.reject( error );
+				});
+
 			});
-
-			return deferred.promise;
-
 		}
 
 	};
